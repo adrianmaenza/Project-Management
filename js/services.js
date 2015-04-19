b3b3c36c2f50a3e65b288b94561d1695476c8063
adrianmaenza <adrianmaenza@gmail.com>
2015-04-19 10:37:00 +0200

    update 2.0
    Added the ability to post a question

--- js/services.js
+++ js/services.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $.get('http://prm.bhakosi.com/queries.php', {query: 'questions'}, function (data) {
-        $('#questions').append(data);
+        $('#questions').html(data);
     });
     
     $(document.body).on('click', '#questions li a', function (e) {
         var id = $(this).data('id');
             e.preventDefault();
-            
+            $('#responses').html('<img src="imgs/load.gif" id="load">');
+        
+            $('#responses').html('<img src="imgs/load.gif" id="load">');
         $.get('http://prm.bhakosi.com/queries.php', {query: 'answers', qid: id}, function (data) {
             $('#responses').html(data);
         });
