111c07569b6bc317d2e2db79f651f73cb9ac2687
adrianmaenza <adrianmaenza@gmail.com>
2015-04-19 11:09:42 +0200

    2.1
    added auto refresh

--- js/services.js
+++ js/services.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-    $.get('http://prm.bhakosi.com/queries.php', {query: 'questions'}, function (data) {
-        $('#questions').html(data);
-    });
+    function get_questions (){
+            $.get('http://prm.bhakosi.com/queries.php', {query: 'questions'}, function (data) {
+            $('#questions').html(data);
+        });
+    }
+    
+    get_questions();
     
     $(document.body).on('click', '#questions li a', function (e) {
         var id = $(this).data('id');
@@ -13,4 +17,21 @@ $(document).ready(function () {
             $('#responses').html(data);
         });
     });
+    
+    $('#pq').on('submit', function (e) {
+        var question = $('#new_question').val(),
+            name = $('#name').val();
+        e.preventDefault();
+        $('#responses').html('<img src="imgs/load.gif" id="load">');
+        $.get('http://prm.bhakosi.com/queries.php', {action: 'post_question', question: question, user: name}, function (data) {
+            if(data){
+                $('#responses').html(data);
+                $('#pq').trigger("reset");
+            }
+        });
+    });
+    
+    setInterval(function (){
+        get_questions();
+    }, 60000);
 });
