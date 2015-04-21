f833b81e7a89f2a1b89c9f3c32adee67ca6de543
adrianmaenza <adrianmaenza@gmail.com>
2015-04-21 13:08:16 +0200

    jquery mobile
    jquery mobile

--- js/services.js
+++ js/services.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     function get_questions (){
             $.get('http://prm.bhakosi.com/queries.php', {query: 'questions'}, function (data) {
             $('#questions').html(data);
+            $('#questions').listview('refresh');
         });
     }
     
@@ -10,11 +11,11 @@ $(document).ready(function () {
     $(document.body).on('click', '#questions li a', function (e) {
         var id = $(this).data('id');
             e.preventDefault();
-            $('#responses').html('<img src="imgs/load.gif" id="load">');
+            $('#atq').html('<img src="imgs/load.gif" id="load">');
         
-            $('#responses').html('<img src="imgs/load.gif" id="load">');
+            $('#atq').html('<img src="imgs/load.gif" id="load">');
         $.get('http://prm.bhakosi.com/queries.php', {query: 'answers', qid: id}, function (data) {
-            $('#responses').html(data);
+            $('#atq').html(data);
         });
     });
     
